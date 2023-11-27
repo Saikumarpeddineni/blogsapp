37b89aeec77518ca4209f4f7566aae48f68267e3
Saikumarpeddineni <saikumarpeddineni18@gmail.com>
2023-11-27 17:22:20 +0530

    updateddd.

--- backend/index.js
+++ backend/index.js
@@ -81,9 +81,12 @@ app.post('/post',uploadMiddleware.single('file'),async (req,res)=>{
     const newPath = path+'.'+ext;
     fs.renameSync(path,newPath);
 
-    // const {token} = req.cookies;
-    // jwt.verify(token,secret,{},async (err,info)=>{
-    //     if(err) throw err;
+    const {token} = req.cookies;
+    jwt.verify(token,secret,{},async (err,info)=>{
+        if (err) {
+            console.error(err);
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
         const {title,summary,content}=req.body;
         const postDoc = await Post.create({
         title,summary,content,
@@ -93,7 +96,7 @@ app.post('/post',uploadMiddleware.single('file'),async (req,res)=>{
 
     res.json(postDoc);
     console.log(res,".jgjukvh");
-    // });
+    });
 });
 
 app.get('/post',async (req,res)=>{
