7f485b12647817543ab356d6e86d89c2e6e8a8f0
Sai Kumar Peddineni <saikumarpeddineni18@gmail.com>
2024-02-10 23:23:03 +0530

    latest

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -12,7 +12,7 @@ export default function Header(){
           setUserInfo(userInfo);
       });
     });
-  },[userInfo]);
+  },[]);
 
     function logout(){
       fetch('https://myblogs-vzdk.onrender.com/logout',{
