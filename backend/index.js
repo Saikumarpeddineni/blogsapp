9f1d31f921c5300d428f71fbca54a218676649b5
Sai Kumar Peddineni <saikumarpeddineni18@gmail.com>
2024-02-11 16:25:03 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -100,9 +100,9 @@ app.post('/post',uploadMiddleware.single('file'),async (req,res)=>{
     const newPath = path+'.'+ext;
     fs.renameSync(path,newPath);
 
-    const {token} = req.cookies;
+    const {token} = localStorage.getItem('token');
     jwt.verify(token,secret,{},async (err,info)=>{
-        if (!token&&(localStorage.getItem("blog")!=="yes")) {
+        if (!token) {
             return res.status(401).json({ error: 'Unauthorized - Token missing' });
         }
         if (err) {
